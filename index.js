65826e9cde2ed7966e98dcc9728ccb7c432e879a
noel nyakundi <noel.nyakundi@student.moringaschool.com>
2024-04-05 01:33:21 +0300

    change

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const header = document.createElement("h4");
+
+header.id = "victory";
+
+header.textContent = "YOUR-NAME is the champion"; 
+
+document.body.appendChild(header);
